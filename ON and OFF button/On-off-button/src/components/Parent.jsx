4607f89bfedd3b8c01e060f9f0b2001ea5797bfa
Mayank Deep <rishusinha525@gmail.com>
2024-08-19 02:44:48 +0530

    added bg change feature

--- ON and OFF button/On-off-button/src/components/Parent.jsx
+++ ON and OFF button/On-off-button/src/components/Parent.jsx
@@ -2,17 +2,27 @@ import {useState} from "react";
 
 import "../css files/parentStyle.css";
 import "../css files/buttonStyle.css";
+import "../css files/bodyStyle.css";
+
+let colorOff = { "backgroundColor" : "white" }
+let colorOn = { "backgroundColor" : "black" }
+
 let parent = () => {
 
     const [isOn , updateIsOn] = useState(false);
 
     return(
 
-        <section id ={(isOn) ? "parent_on" : "parent_off"} >
+        <div className="bg-body " style={(isOn) ? colorOn : colorOff}>
 
-            <button onClick={() => updateIsOn(!isOn)} id = {(isOn) ? "button_on" : "button_off"} > { (isOn) ? "ON" : "OFF" }</button>
+            <section id ={(isOn) ? "parent_on" : "parent_off"} >
 
-        </section>
+                <button onClick={() => updateIsOn(!isOn)} id = {(isOn) ? "button_on" : "button_off"} > { (isOn) ? "ON" : "OFF" }</button>
+
+            </section>
+
+        </div>
+        
     );
 }
 
